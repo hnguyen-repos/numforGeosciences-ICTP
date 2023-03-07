7e0e06f813b029a38f8d569725533731b181b504
ben <sipkaro@gmail.com>
2023-03-07 19:06:22 +0100

    Auto Commit

--- lec1.py
+++ lec1.py
@@ -1,11 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import netCDF4 as nc
 from mpl_toolkits.basemap import Basemap
-
-file1= '/home/ben/ictp/numforGeosciences-ICTP/datasets/lec1-sample.txt'
-data = np.loadtxt(file1)
 
 file2 = "/home/ben/ictp/numforGeosciences-ICTP/datasets/lec1-era5_tas_1959_2021_25deg.nc"
 f1 = nc.Dataset(file2)
@@ -13,13 +9,11 @@ lon = f1.variables['lon'][:]
 lat = f1.variables['lat'][:]
 var = f1.variables['t2m'][:,:,:]
 varAverage = np.average(var, axis = 0)
-
 lon, lat = np.meshgrid(lon, lat)
 
 for i in range(756):
     m = Basemap(llcrnrlat=-80,urcrnrlat=80,llcrnrlon=0,urcrnrlon=360)
     m.drawcoastlines()
     m.pcolormesh(lon, lat, var[i,:,:],latlon=True, cmap='RdBu_r')
-    # time.sleep(0.1)
     plt.pause(0.05)
 plt.show()
